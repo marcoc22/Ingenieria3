0fb444abcd60bdc202215893e4aee2fd59ff835c
marcoc22 <mtcanalesmesen@gmail.com>
2017-02-25 16:16:56 -0600

    listadoDecomisos

--- SistemaDecomisos/web/js/listaDecomisos.js
+++ SistemaDecomisos/web/js/listaDecomisos.js
@@ -1,6 +1,21 @@
 var errores = new Array();
 
 $(document).ready(function () {
-    $("#decomisosTable").dataTable();
-    
+    //$("#decomisosTable").dataTable();
+    var table = $('#decomisosTable').dataTable()
+            .fnAddData([
+                "2",
+                "2017-01-05",
+                "11:47:56 PM",
+                "Heredia",
+                "Mario Arias",
+                "Luis Mora Mora",
+                "Perecedero",
+                "<button>Acta Donacion</button>"
+            ]); 
+    /*$('#decomisosTable tbody').on('click', 'button', function () {
+        var data = table.row($(this).parents('tr')).data();
+        alert(data[2]);
+    });*/
+
 });
